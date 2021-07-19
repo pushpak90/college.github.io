4f0a9231ef452b9d72334745e3f69fab27ffaa29
pushpak90 <pushpakafasate90@gmail.com>
2021-07-19 22:14:51 +0530

    php runserver

--- form-handler.php
+++ form-handler.php
@@ -2,13 +2,24 @@
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
-$subject = $_POST['message'];
+$message = $_POST['message'];
+  
+$email_form = 'pushpakafasate90@gmail.com';
 
-$email_form = ''
+$email_subject= 'New Form Submission';
 
+$email_body = "User Name : $name.\n".
+                "User Email : $visitor_email.\n".
+                    "Subject : $subject.\n".
+                    "User message : $message.\n";
 
+$to = 'pushpakafasate90@gmail.com';
 
+$headers = "Form: $email_form \r\n";
 
+$headers .="Reply-To: $visitor_email\r\n";
 
+mail($to,$email_subject,$email_body,$headers);
 
+header("Location: contact.html");
 ?>
